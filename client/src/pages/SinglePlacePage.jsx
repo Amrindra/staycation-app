f68021f377ca3987d2d534a76974f3162f1f6b62
Amrindra Ouk <amrindraouk@yahoo.com>
2023-08-31 15:09:32 -0400

    Working on show all more photos

--- client/src/pages/SinglePlacePage.jsx
+++ client/src/pages/SinglePlacePage.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 const SinglePlacePage = () => {
   const { id } = useParams();
-  const [place, setPlace] = useState(null);
+  const [places, setPlaces] = useState(null);
   const [showAllPhotos, setShowAllPhots] = useState(false);
 
   useEffect(() => {
@@ -12,55 +12,117 @@ const SinglePlacePage = () => {
       return;
     }
     axios.get(`/places/${id}`).then((response) => {
-      setPlace(response.data);
+      setPlaces(response.data);
     });
   }, [id]);
 
-  if (!place) {
+  if (!places) {
     return "Loading";
   }
 
+  console.log(places);
+
   if (showAllPhotos) {
-    return <div className="fixed bg-white min-w-full min-h-screen"></div>;
+    return (
+      <div className="absolute bg-white inset-0">
+        <div className="p-8 grid gap-4">
+          <div>
+            <h3 className="text-3xl">Photos of {places.title}</h3>
+            <button
+              onClick={() => setShowAllPhots(false)}
+              className="fixed  flex gap-1 p-2 rounded-full bg-[#F5385D] text-white shadow shadow-black"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          {places?.photos?.length > 0 &&
+            places.photos.map((photo) => (
+              <div
+                key={photo._id}
+                // className="grid lg:grid-cols-3 md:grid-cols-2"
+              >
+                <img
+                  src={`http://localhost:8000/uploads/${photo}`}
+                  alt=""
+                  // className="w-full object-cover"
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="mt-8 bg-gray-100 -mx-8 px-8 py-8">
-      <h1 className="text-2xl">{place.title}</h1>
+      <h1 className="text-2xl">{places.title}</h1>
       <a
         target="_blank"
         rel="noreferrer"
-        href={"https://maps.google.com/?q=" + place.address}
-        className="block font-semibold underline my-2"
+        href={"https://maps.google.com/?q=" + places.address}
+        className="flex gap-1 font-semibold underline my-3"
       >
-        {place.address}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
+
+        {places.address}
       </a>
 
       {/* Photos section */}
       <div className="relative">
         <div className="grid gap-2 grid-cols-[2fr_1fr]">
           <div>
-            {place.photos?.[0] && (
+            {places.photos?.[0] && (
               <img
-                src={`http://localhost:8000/uploads/${place.photos[0]}`}
+                src={`http://localhost:8000/uploads/${places.photos[0]}`}
                 alt=""
                 className="object-cover aspect-square w-full"
               />
             )}
           </div>
           <div className="grid gap-2">
-            {place.photos?.[0] && (
+            {places.photos?.[0] && (
               <img
-                src={`http://localhost:8000/uploads/${place.photos[1]}`}
+                src={`http://localhost:8000/uploads/${places.photos[1]}`}
                 alt=""
                 className="object-cover aspect-square w-full"
               />
             )}
 
             <div className="overflow-hidden">
-              {place.photos?.[0] && (
+              {places.photos?.[0] && (
                 <img
-                  src={`http://localhost:8000/uploads/${place.photos[2]}`}
+                  src={`http://localhost:8000/uploads/${places.photos[2]}`}
                   alt=""
                   className="object-cover aspect-square w-full relative t-2"
                 />
@@ -70,7 +132,7 @@ const SinglePlacePage = () => {
         </div>
         <button
           onClick={() => setShowAllPhots(true)}
-          className="flex gap-1 absolute right-0 bottom-0 px-4 py-2 border border-black bg-white rounded-xl shadow-md shadow-black"
+          className="flex gap-1 absolute right-0 bottom-0 px-4 py-2 border border-black bg-[#F5385D] text-white rounded-xl shadow-md shadow-black"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
